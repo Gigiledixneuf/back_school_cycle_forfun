f26d9f04a3b14ed5beb6a5860d0998f8b8080239
Guerchom Kubaluka <guerchomkubaluka@gmail.com>
2025-05-29 02:31:25 +0100

    Creation du model, migration et controller Photo

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\AnnouncementResource;
 use App\Models\Announcement;
+use App\Models\Photo;
 use App\Models\User;
 use App\Notifications\NewAnnouncementNotification;
 use Illuminate\Http\Request;
@@ -15,7 +16,12 @@ class AnnouncementController extends Controller
     //function pour voir toutes les annonces disponible
     public function index()
     {
-        return AnnouncementResource::collection(Announcement::where('is_completed', false)->where('is_cancelled', false)->orderBy('created_at', 'desc')->get() );
+        return AnnouncementResource::collection(
+            Announcement::where('is_completed', false)
+                ->where('is_cancelled', false)
+                ->orderBy('created_at', 'desc')
+                ->paginate(10)
+        );
     }
 
 
@@ -37,34 +43,67 @@ class AnnouncementController extends Controller
     public function store(Request $request)
     {
         //on recupere le user connecter
-        $user=Auth::user();
+        $user = Auth::user();
+
         try {
-            $validated = $request->validate([
+            if ($user->role !== 'tutor') {
+                return response()->json([
+                    "Message" => "Vous n'avez pas le droit de creer une annonce"
+                ]);
+            }
+            $request->validate([
                 'title' => 'required|string|min:5|max:500',
                 'description' => 'required|string|max:1000',
-                'category_id'=>'required|exists:categories,id',
+                'category_id' => 'required|exists:categories,id',
                 'operation_type' => 'required|string|in:don,sale,exchange',
+                'state' => 'required|string|in:new,good,damaged,like new',
                 'price' => 'nullable|numeric',
                 'is_completed' => 'nullable|boolean',
                 'is_cancelled' => 'nullable|boolean',
                 'exchange_location_address' => 'string|max:255',
                 'exchange_location_lng' => 'numeric',
                 'exchange_location_lat' => 'numeric',
-                'created_by' => 'required|exists:users,id'
+                'photos.*' => 'required|image|mimes:jpg,png,gif|max:2040'
             ]);
 
-            $announcement = Announcement::create($validated);
+            $announcement = Announcement::create([
+                'title' => $request['title'],
+                'description' => $request['description'],
+                'category_id' => $request['category_id'],
+                'operation_type' => $request['operation_type'],
+                'state'=>$request['state'],
+                'price' => $request['price'],
+                'is_completed' => $request['is_completed'] ?? false,
+                'is_cancelled' => $request['is_cancelled'] ?? false,
+                'exchange_location_address' => $request['exchange_location_address'],
+                'exchange_location_lng' => $request['exchange_location_lng'],
+                'exchange_location_lat' => $request['exchange_location_lat'],
+                'created_by' => $user->id
 
-            // recherche des utilisateurs
-            $users = User::whereHas('preferences',function($query) use ($announcement){
-                $query->where('categories.id',$announcement->category_id);
+            ]);
+
+            if ($request->hasFile('photos')) {
+                foreach ($request->file('photos') as $image) {
+                    $path = $image->store('announcements', 'public');
+
+                    Photo::create([
+                        'announcement_id' => $announcement->id,
+                        'url' => $path,
+                    ]);
+                }
+            }
+
+            $announcement->load(['category', 'user', 'favorites', 'photos']);
+
+            $users = User::whereHas('preferences', function ($query) use ($announcement) {
+                $query->where('categories.id', $announcement->category_id);
             })->get();
 
 
             // verification si il n'a trouvé aucun utilisateur
-            if($users->count() !== 0){
+            if ($users->count() !== 0) {
                 //Envoi des mail aux utilisateurs
-                Notification::send($users,new NewAnnouncementNotification($announcement));
+                Notification::send($users, new NewAnnouncementNotification($announcement));
             }
 
             return new AnnouncementResource($announcement);
@@ -74,8 +113,8 @@ class AnnouncementController extends Controller
                 'Erreur' => $exception->getMessage()
             ]);
         }
-
     }
+
 
     //function pour mettre à une annonce
     public function update(Request $request, Announcement $announcement)
@@ -84,22 +123,23 @@ class AnnouncementController extends Controller
         try {
 
             //on verifie si l'utilisateur connecter est l'auteur de l'article
-            if ($user->id !== $announcement->user_id) {
+            if ($user->id !== $announcement->created_by) {
                 return response()->json([
                     'Message' => "Vous n'avez pas le droit de modifier cette annonce"
                 ], 403);
             } else {
-                $validated =$request->validate([
+                $validated = $request->validate([
                     'title' => 'required|string|min:5|max:500',
-                    'descirption' => 'required|string|max:1000',
+                    'description' => 'required|string|max:1000',
                     'operation_type' => 'required|string|in:don,sale,exchange',
+                    'state' => 'required|string|in:new,good,damaged,like new',
                     'price' => 'nullable|numeric',
                     'is_completed' => 'nullable|boolean',
                     'is_cancelled' => 'nullable|boolean',
                     'exchange_location_address' => 'string|max:255',
                     'exchange_location_lng' => 'numeric',
                     'exchange_location_lat' => 'numeric',
-                    'category_id' => 'required|exists:category,id'
+                    'category_id' => 'required|exists:categories,id'
                 ]);
 
                 $announcement->update($validated);
@@ -121,7 +161,7 @@ class AnnouncementController extends Controller
 
         try {
 
-            if ($user->id !== $announcement->user_id) {
+            if ($user->id !== $announcement->created_by) {
                 return response()->json([
                     'Message' => "Vous n'avez pas le droit de supprimer cette annonce",
                 ], 403);
--- resources/views/mail/user-registered-mail.blade.php
+++ resources/views/mail/user-registered-mail.blade.php
@@ -1,19 +1,19 @@
 <x-mail::message>
-    # 🎉 Vérifiez votre adresse e-mail
+# 🎉 Vérifiez votre adresse e-mail
 
-    Bonjour **{{ $user->name }}**,
+Bonjour **{{ $user->name }}**,
 
-    Merci beaucoup pour votre inscription sur **{{ config('app.name') }}** !
-    Pour finaliser votre inscription et accéder à toutes les fonctionnalités, veuillez vérifier votre adresse e-mail en cliquant sur le bouton ci-dessous :
+Merci beaucoup pour votre inscription sur **{{ config('app.name') }}** !  
+Pour finaliser votre inscription et accéder à toutes les fonctionnalités, veuillez vérifier votre adresse e-mail en cliquant sur le bouton ci-dessous :
 
-    <x-mail::button :url="$url" color="primary">
-        Vérifier mon adresse e-mail
-    </x-mail::button>
+<x-mail::button :url="$url" color="primary">
+Vérifier mon adresse e-mail
+</x-mail::button>
 
-    Ce lien expirera dans **60 minutes**.
+Ce lien expirera dans **60 minutes**.
 
-    Si vous n’avez pas créé de compte, vous pouvez simplement ignorer cet e-mail.
+Si vous n’avez pas créé de compte, vous pouvez simplement ignorer cet e-mail.
 
-    Merci,
-    L’équipe **{{ config('app.name') }}**
+Merci,  
+L’équipe **{{ config('app.name') }}**
 </x-mail::message>
